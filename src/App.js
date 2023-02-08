c350a804db2a374d0343410243590194864b0f26
Aliceson Phillips <alicesonphillips@gmail.com>
2023-02-07 20:53:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { Router, Route, Link } from "react-router-dom";
+import Pizza from "./Components/Pizza";
+import Blogs from "./Components/Blogs"
+
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+  <Router>
+     <h1>BloomTech Eats</h1>
+        <div className="App"> 
+          <p>Order the best pizza you will ever taste!</p>
+           <nav>
+            <li>
+            <Link to ="/pizza"> Order Now!  </Link>
+            </li>
+            <li>
+            <Link to ="/blogs"> Blogs </Link>
+            </li>
+           </nav>
+
+
+            <button>
+              <Route path ="/pizza" component={Pizza} />
+              <Route path ="/blogs" component={Blogs} />
+            </button>
+
+      </div>
+    </Router>
   );
 };
 export default App;
