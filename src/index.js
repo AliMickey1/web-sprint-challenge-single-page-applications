71437f30e049baca701d7ddc953a59df958d76cb
Aliceson Phillips <alicesonphillips@gmail.com>
2023-02-09 05:24:11 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
-// import App from "./App";
-import Pizza from "./Components/Pizza"
+import App from "./App";
+// import Pizza from "./Components/Pizza"
 
 ReactDOM.render(
 <Router>
-<Pizza />
+<App />
 </Router>
 , document.getElementById("root"));
