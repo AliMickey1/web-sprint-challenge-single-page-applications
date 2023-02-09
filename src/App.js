55b20abf8990b517d1abbdc501c2727e04b29c32
Aliceson Phillips <alicesonphillips@gmail.com>
2023-02-09 05:24:03 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React from "react";
-import { Router, Route, Link } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import Pizza from "./Components/Pizza";
-import Blogs from "./Components/Blogs"
+
+
 
 
 const App = () => {
+
+
+
   return (
-  <Router>
+    
+    <div className="App">  
+    <nav>
      <h1>BloomTech Eats</h1>
-        <div className="App"> 
-          <p>Order the best pizza you will ever taste!</p>
-           <nav>
+         <p>Order the best pizza you will ever taste!</p>
+
             <li>
             <Link to ="/pizza"> Order Now!  </Link>
-            </li>
-            <li>
-            <Link to ="/blogs"> Blogs </Link>
             </li>
            </nav>
 
 
-            <button>
+
               <Route path ="/pizza" component={Pizza} />
-              <Route path ="/blogs" component={Blogs} />
-            </button>
+
 
       </div>
-    </Router>
+
   );
 };
 export default App;
