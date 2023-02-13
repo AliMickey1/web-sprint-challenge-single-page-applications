0dcd1d430701a133141a249e9accf5a21e8d2f33
Aliceson Phillips <alicesonphillips@gmail.com>
2023-02-13 10:18:45 -0500

    save and check errors

--- src/App.js
+++ src/App.js
@@ -1,78 +1,105 @@
 import React, { useState, useEffect } from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import Pizza from "./Components/Pizza";
-// import schema from './validation/formSchema'
-// import * as yup from 'yup';
-// import axios from 'axios';
+import schema from './Components/formSchema'
+import * as yup from 'yup';
+import axios from 'axios';
 
 
-// const initialValue = {
-//   cname: ''
-//   };
+const initialValue = {
+  'name-input': '',
+  'size-dropdown': '',
+  'pizza-sauce': '', 
+  'pepperoni': false,
+  'sausage': false,
+  'canadianbacon': false,
+  'spicyitalsaus': false,
+  'grilledchix': false,
+  'onions': false,
+  'greenpeppers': false,
+  'tomatoes': false,
+  'olives': false,
+  'garlic': false,
+  'artichoke': false,
+  '3cheese': false,
+  'pineapple': false,
+  'xtracheese': false,
+  'special-text': ''
+  };
 
 
-//   const initialFormErrors = {
-//       cname: ''
-//       };
+  const initialFormErrors = {
+    'name-input': '',
+    'size-dropdown': '',
+    'pizza-sauce': '', 
+      };
+
+  const initialOrder = []
+  const intialDisabled = true
 
 const App = () => {
 
 
-// const [formValues, setFormValues] = useState(initialValue);
-// const [formErrors, setFormErrors] = useState(initialFormErrors);
-// const [order, setOrder] = useState([]); //returns a database record of name, size, toppings and special instructions
+const [formValues, setFormValues] = useState(initialValue);
+const [formErrors, setFormErrors] = useState(initialFormErrors);
+const [orders, setOrders] = useState(initialOrder); //returns a database record of name, size, toppings and special instructions
+const [disabled, setDisabled] = useState(intialDisabled)
 
-// const formSubmit = e => {
+const getOrder = () => {
+  axios.get('http://regres.in/api/orders')
+    .then(res=> {
+      setOrders(res.data)
+    })
+    .catch(err =>  console.log(err))
+}
 
-//     axios.post('https://reqres.in/api/orders', formValues)
-//     .then(res => {
-//       // setOrders([res.data, ...orders] )
-//       // <pre>{JSON.stringify(setOrders(res.data, ...orders ))}</pre>
-//       console.log("success", res);
-//     })
-//     .catch(err => console.error(err))
-// }
+const formSubmit = e => {
+
+    axios.post('https://reqres.in/api/orders', formValues)
+    .then(res => {
+
+      setOrders([res.data, ...orders] )
+      setFormValues(initialValue)
+    })
+    .catch(err => console.error(err))
+}
 
 
-// const validate = (name, value) => {
-//   yup.reach(schema, name)
-//   .validate(value)
-//   .then(() => setFormErrors({ ...setFormErrors, [name]: '' }))
-//   .catch(err => setFormErrors({ ...setFormErrors, [name]: err.errors[0] }))
-// }
+const validate = (name, value) => {
+  yup.reach(schema, name)
+  .validate(value)
+  .then(() => setFormErrors({ ...setFormErrors, [name]: '' }))
+  .catch(err => setFormErrors({ ...setFormErrors, [name]: err.errors[0] }))
+}
 
 
 
-// const handleChange = (name, value) => {
-//   validate(name, value);
-//   setFormValues({...formValues, [name]: value});
-// }
+const handleChange = (name, value) => {
+  validate(name, value);
+  setFormValues({...formValues, [name]: value});
+}
 
 
   return (
     
+
     <div className="App">  
     <nav>
      <h1>BloomTech Eats</h1>
          <p>Order the best pizza you will ever taste!</p>
+        <div className="nav-links">
+           <button id="order-pizza">
+              <Link to ="/pizza"> Order Now!  </Link>
+           </button>
+        </div>
+        </nav>
 
-            <li>
-              {/* <button id="home">
-                <Link to ="/">Home</Link>
-                <Route exact path = "/" component={App}></Route>
-              </button> */}
-              <button id="order-pizza">
-                <Link to ="/pizza"> Order Now!  </Link>
-                <Route path ="/pizza" component={Pizza} />
-            </button>
-            </li>
-           </nav>
-
-
-
-
+      <Switch>
+          <Route path ="/pizza"> <Pizza /></Route>
+      </Switch>
       </div>
 
-  );
+
+ );
 };
 export default App;
